ec56c876b6124ba4ac39f3e318a4f6e4d4f58736
freddynhon <55215075+freddynhon@users.noreply.github.com>
2022-08-19 01:39:52 -0500

    Generales
    Header, fuentes y menú interno.

--- app/components/layout/EnterpriseTabMenu.vue
+++ app/components/layout/EnterpriseTabMenu.vue
@@ -1,18 +1,24 @@
-<template>
-<StackLayout>
-  <label text="Noticias"></label>
-  <label text="Nuevo en la oficina"></label>
-  <label text="Indicadores"></label>
-
-  <!-- <button>Noticias</button>
-  <button>Nuevo en la oficina</button>
-  <button>Indicadores</button> -->
-<GridLayout>
-  <Section v-if="selectedItem == 0" sect="5"></Section>
-  <NewInOffice v-if="selectedItem == 1"></NewInOffice>
-  <IndicatorsPage v-if="selectedItem == 2"></IndicatorsPage>
-</GridLayout> 
-</StackLayout>
+<template>	
+	<StackLayout>
+		<ScrollView orientation="horizontal" height="76" class="enterprise-gral">
+			<GridLayout columns="auto auto auto auto auto auto auto auto auto">
+				<label column="0" text="Noticias" class="enterprise-menu enterprise-active" textWrap="true"></label>
+				<label column="1" text="Nuevo en la oficina" class="enterprise-menu" textWrap="true"></label>
+				<label column="2" text="Indicadores" class="enterprise-menu" textWrap="true"></label>
+				<label column="3" text="Todos" class="enterprise-menu" textWrap="true"></label>
+				<label column="4" text="Comment LR" class="enterprise-menu" textWrap="true"></label>
+				<label column="5" text="Datos cocteleros" class="enterprise-menu" textWrap="true"></label>
+				<label column="6" text="Mercado cambiario" class="enterprise-menu" textWrap="true"></label>
+				<label column="7" text="Macro" class="enterprise-menu" textWrap="true"></label>
+				<label column="8" text="Bancos" class="enterprise-menu" textWrap="true"></label>
+			</GridLayout>
+		</ScrollView>
+		<GridLayout>
+			<Section v-if="selectedItem == 0" sect="5"></Section>
+			<NewInOffice v-if="selectedItem == 1"></NewInOffice>
+			<IndicatorsPage v-if="selectedItem == 2"></IndicatorsPage>
+		</GridLayout>
+	</StackLayout>	
 </template>
 
 <script>
@@ -32,3 +38,30 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.enterprise-gral {
+	margin: 0 6px;
+}
+.enterprise-menu {
+	border-color: #C2C2C2;
+	border-width: 1 1 1 1;
+	height: 76px;
+	padding: 11px 21px;
+	text-align: center;
+	font-size: 13px;
+	line-height: 16px;
+	font-family: Montserrat;
+	font-weight: 500;
+	color: #808080;
+	border-radius: 36px;
+	margin-left: 6px;
+	margin-right: 6px;
+}
+
+.enterprise-active {
+	color: #C51A1B;
+	background-color: #EEEEEE;
+	border-color: #EAEAEA;
+}
+</style>
--- app/components/layout/Main.vue
+++ app/components/layout/Main.vue
@@ -1,16 +1,26 @@
 <template>
     <Page class="page">
     
-      <ActionBar class="action-bar">
+      <!--<ActionBar class="action-bar">
         <NavigationButton visibility="hidden"/>
 
         <GridLayout columns="50, *">
           <Image src="~/assets/logo.png" width="39"
                 verticalAlignment="center" />
 
-          <!--<Label class="fas" text.decode="&#xf0c9;" @tap="onDrawerButtonTap"/>-->
+          <Label class="fas" text.decode="&#xf0c9;" @tap="onDrawerButtonTap"/>
         </GridLayout>
-      </ActionBar>
+      </ActionBar>-->
+      <ActionBar backgroundColor="#fff" flat="true">
+        <StackLayout orientation="horizontal">
+          <Label class="fas icon-menu" text.decode="&#xf0c9;" @tap="onDrawerButtonTap"/>
+          
+            <Image src="~/assets/logo.png" width="39"
+                verticalAlignment="center" />   
+                <Label class="far icon-search-top" text.decode="&#xf002;"/> 
+                <Label class="far" text.decode="&#xf007;"/>  
+        </StackLayout>
+    </ActionBar>
         <MainBottomMenu></MainBottomMenu>
        
     </Page>

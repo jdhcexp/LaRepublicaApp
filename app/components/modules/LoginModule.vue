1b13f5f104f5d8996bda925b7a7333b0892c8f0d
freddynhon <55215075+freddynhon@users.noreply.github.com>
2022-09-04 20:22:09 -0500

    Login - Mis Noticias
    Se estiliza la sección de login de "Mis Noticias".

--- app/components/modules/LoginModule.vue
+++ app/components/modules/LoginModule.vue
@@ -1,21 +1,21 @@
 <template>
-  <StackLayout>
-    <label text="MI SELECCIÓN DE NOTICIAS"></label>
-    <label text="Vea las noticias personalizadas de acuerdo con su top 20 de los temas de interes"></label>
-
-    <label text="Si usted es suscriptor puede"></label>
-    <button @tap="onLoginTap()">INICIAR SESIÓN.</button>
-    <label text="o de lo contrario"></label>
-    <button @tap="onLogoutTap()">SUSCRÍBASE AQUÍ</button>
-    <label :text="getToken"></label>
-  </StackLayout>
-</template>
-    
-    <script>
+    <StackLayout>
+		<Image src="~/assets/icons/noticias_gray.png" width="85" class="logo-news" />
+    	<label text="MI SELECCIÓN DE NOTICIAS" class="title-news"></label>
+    	<label text="Vea las noticias personalizadas, de acuerdo con su top 20 de los temas de interés" class="description-news" textWrap="true"></label>
+		<StackLayout class="login-box">
+			<label text="Si usted es suscriptor puede" class="instruction"></label>
+			<button class="btn-iniciar" @tap="onLoginTap()">INICIAR SESIÓN</button>
+			<label text="o de lo contrario" class="instruction"></label>
+			<button @tap="onLogoutTap()" class="btn-subscribe">SUSCRÍBASE AQUÍ</button>
+			<label :text="getToken"></label>
+		</StackLayout>
+	</StackLayout>
+</template>  
+<script>
     // var auth_service_1 = require("../../auth-service");
     import { ApplicationSettings } from '@nativescript/core';
-    
-    
+      
     export default {
     
       computed: {
@@ -33,4 +33,49 @@
         }
       }
     }
-    </script>
+</script>
+<style>
+	.logo-news {
+		text-align: center;
+		margin-top: 226px;
+	}
+	.title-news {
+		text-align: center;
+		font: normal normal bold 19px/25px Montserrat;
+		color: #000000;
+		margin-top: 52px;
+	}
+	.description-news {
+		text-align: center;
+		font-size: 13px;
+  		font-family: 'Montserrat', sans-serif;
+  		font-weight: 400;
+		color: #000000;
+		margin: 22px 106px;
+	}
+	.login-box {
+		border-color: #EEEEEE;
+  		border-width: 1 1 1 1;
+		margin: 78px 106px;
+		padding: 58px 52px;
+	}
+	.instruction {
+		text-align: center;
+		font-size: 13px;
+  		font-family: 'Montserrat', sans-serif;
+  		font-weight: 400;
+		color: #000000;
+	}
+	.btn-iniciar {
+		border-color: #000000;
+  		border-width: 1 1 1 1;
+		background-color: #EEEEEE;
+		font: normal normal bold 13px/17px Montserrat;
+		color: #000000;
+	}
+	.btn-subscribe {
+		background-color: #C51A1B;
+		font: normal normal bold 13px/17px Montserrat;
+		color: #FFFFFF;
+	}
+</style>

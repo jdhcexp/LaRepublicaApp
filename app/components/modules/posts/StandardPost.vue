3115792d0ef79daba443ddff7e404e60569c8c73
freddynhon <55215075+freddynhon@users.noreply.github.com>
2022-09-05 00:17:57 -0500

    Post interno
    Se arregla la visual del post interno de noticias con el diseño final.

--- app/components/modules/posts/StandardPost.vue
+++ app/components/modules/posts/StandardPost.vue
@@ -1,27 +1,29 @@
 <template>
-  <Page class="page">
-    <ScrollView>
-      <label :text="postId"></label>
-      <GridLayout rows="250 1000" v-if="post">
-        <WrapLayout row="0" backgroundColor="#fff">
-          <Label :text="post.header" class="category" />
-          <AbsoluteLayout backgroundColor="#fff">
-            <Image :src="post.principalImage.url" top="55" class="img-ppal" />
-            <StackLayout class="titular-box">
-              <label :text="post.title" class="titular-ppal" textWrap="true" />
-              
-            </StackLayout>
-          </AbsoluteLayout>
-        </WrapLayout>
+	<Page class="page">
+		<ScrollView>
+		<label :text="postId"></label>
+		<GridLayout rows="470 1000" v-if="post">
+			<WrapLayout row="0" backgroundColor="#fff">
+				<Label :text="post.header" class="category" />
+				<label :text="post.title" class="titular-into" textWrap="true" />
+				<Label text="2017/11/06" class="date" />
+				<AbsoluteLayout backgroundColor="#fff">
+					<Image :src="post.principalImage.url" top="0" class="img-ppal" />
+				</AbsoluteLayout>
+				<StackLayout orientation="horizontal" class="post-foot">
+					<Image src="~/assets/icons/camara.png" width="15" class="" verticalAlignment="center" />
+					<Label text="Bloomberg" class="text-bloom" />
+				</StackLayout>
+			</WrapLayout>
 
-        
-        <WebView row="1" :src="post.content" height="1000"/>
-      </GridLayout>
+			
+			<WebView row="1" :src="post.content" height="1000"/>
+		</GridLayout>
 
-    </ScrollView>
-    <!-- <label :text="post.header" class="category" /> -->
-    <!-- </GridLayout>  -->
-  </Page>
+		</ScrollView>
+		<!-- <label :text="post.header" class="category" /> -->
+		<!-- </GridLayout>  -->
+	</Page>
   <!-- <Page>
     
       <GridLayout rows="75 * 50">
@@ -85,30 +87,52 @@ export default {
   },
   props: ['postId']
 }
-
 </script>
-
 <style scoped>
-.category {
-  width: 100%;
-  text-align: center;
-  margin-top: 17px;
-  font-size: 10px;
-  color: #808080;
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 600;
-}
-
-.titular-box {
-  margin-left: 9%;
-  width: 82%;
-  background-color: #fff;
-}
-
-.titular-ppal {
-  text-align: center;
-  font-size: 19px;
-  color: #000;
-  font-family: 'Bitter', serif;
-}
+	.category {
+		width: 100%;
+		text-align: center;
+		margin-top: 17px;
+		font-size: 10px;
+		color: #808080;
+		font-family: 'Montserrat', sans-serif;
+		font-weight: 600;
+	}
+	.titular-into {
+		width: 80%;
+		margin-left: 10%;
+		text-align: center;
+		font-size: 19px;
+		color: #000;
+		font-family: 'Bitter', serif;
+	}
+	.date {
+		width: 100%;
+		text-align: center;
+		margin-top: 16px;
+		font-size: 12px;
+		color: #808080;
+		font-family: 'Montserrat', sans-serif;
+		font-weight: 500;
+		line-height: 16px;
+		margin-bottom: 36px;
+	}
+	.post-foot {
+		background-color: #F5F4F6;
+		margin-left: 32px;
+		margin-right: 32px;
+		height: 86px;
+		padding: 23px 30px;
+		width: 95%;
+		margin-top: -43px;
+		z-index: 99;
+	}
+	.text-bloom {
+		font: normal normal 600 10px/13px Montserrat;
+		color: #808080;
+		border-color: #C2C2C2;
+  		border-width: 0 0 0 1;
+		padding-left: 22px;
+		margin-left: 22px;
+	}
 </style>

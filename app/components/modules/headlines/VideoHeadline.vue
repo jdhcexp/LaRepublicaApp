d1cd81a6d81708eebcdbdb45a0a96e16af075166
freddynhon <55215075+freddynhon@users.noreply.github.com>
2022-09-04 22:56:13 -0500

    Videos
    Se modifica plantilla de Videos, incluyendo el icono de "Play".

--- app/components/modules/headlines/VideoHeadline.vue
+++ app/components/modules/headlines/VideoHeadline.vue
@@ -1,9 +1,17 @@
 <template>
-<StackLayout orientation="horizontal" class="content-news">
-    <Image :src="headline.youtubeThumb" class="thumbnail"/>
+<StackLayout orientation="horizontal" class="content-videos">
+    <AbsoluteLayout backgroundColor="#fff">          
+        <Image :src="headline.youtubeThumb" top="0" class="thumbnail-video"/>
+        <StackLayout class="box-play-int">
+            <Image src="~/assets/icons/play.png" width="40" />
+        </StackLayout>
+    </AbsoluteLayout>   
     <StackLayout>
         <Label :text="headline.header" class="category category-thumb" />
-        <label :text="headline.title" textWrap="true" class="text-thumb" />
+        <label :text="headline.title" textWrap="true" class="text-thumb-video" />
+        <StackLayout orientation="horizontal">
+            <Label class="box-hash-mini" text="DATOS COCTELEROS" textWrap="true"/>
+        </StackLayout>
     </StackLayout>
 </StackLayout>
 </template>
@@ -20,29 +28,51 @@ export default {
     }
 };
 </script>
-
 <style scoped>
-.content-news {
-    margin-top: 22px;
-    margin-left: 15px;
-}
+    .content-videos {
+        margin-top: 54px;
+        margin-left: 32px;
+        margin-right: 32px;
+        position: relative;
+    }
+    .play {
+        position: absolute;
+        top: 50%;
+        left: 0%;
+    }
+    .thumbnail-video {
+        width: 312px;
+        margin-right: 15px;
+        vertical-align: top;
+    }
 
-.thumbnail {
-    width: 26%;
-    margin-right: 15px;
-    vertical-align: top;
-}
+    .category-thumb {
+        text-align: left;
+        margin-top: 0;
+    }
 
-.category-thumb {
-    text-align: left;
-    margin-top: 0;
-}
-
-.text-thumb {
-    margin-top: 5px;
-    font-size: 14px;
-    color: #000;
-    font-family: 'Bitter', serif;
-    padding-right: 15px;
-}
+    .text-thumb-video {
+        margin-top: 5px;
+        font-size: 14px;
+        color: #000;
+        font-family: 'Bitter', serif;
+        padding-right: 15px;
+        margin-left: 32px;
+    }
+    .box-hash-mini {
+        font-size: 11px;
+  		font-family: 'Montserrat', sans-serif;
+  		font-weight: 500;
+		line-height: 14px;
+        color: #808080;
+        border-color: #C2C2C2;
+        border-width: 1 1 1 1;
+        padding: 14px 40px;
+        margin: 24px 32px 0;
+    }
+    .box-play-int {
+        margin-top: 30px;
+        width: 312px;
+        text-align: center;
+    }
 </style>
--- app/components/modules/headlines/VideoMainHeadline.vue
+++ app/components/modules/headlines/VideoMainHeadline.vue
@@ -2,14 +2,20 @@
     <WrapLayout backgroundColor="#fff" v-if="headline">
         <!-- <Label :text="opening.header" class="category" /> -->
          <button @tap="showInfo">qwertyui</button>
-        <AbsoluteLayout backgroundColor="#fff">
-            <Image :src="headline.youtubeThumb" top="55" class="img-ppal" />
-            <StackLayout class="titular-box">
-                <!-- <label v-if="headline.videoShow != null" :text="headline.videoShow.name" class="titular-ppal" textWrap="true" />
-                <label :text="headline.section.name" class="titular-ppal" textWrap="true" /> -->
-                <label :text="headline.title" class="titular-ppal" textWrap="true" />
+        <AbsoluteLayout backgroundColor="#fff">          
+            <Image :src="headline.youtubeThumb" top="0" class="img-ppal" />
+            <StackLayout class="box-play">
+                <Image src="~/assets/icons/play.png" width="50" />
             </StackLayout>
         </AbsoluteLayout>
+        <StackLayout orientation="horizontal">
+            <Label class="box-hash" text="COMMER LR"/>
+			<Label class="box-category" text="HACIENDA"/>
+            <!--<StackLayout class="save">
+                <Image src="~/assets/icons/guardar-relleno.png" width="11" verticalAlignment="center" />
+            </StackLayout>-->
+        </StackLayout>
+        <label :text="headline.title" class="titular-videos" textWrap="true" />
     </WrapLayout>
     <!-- <WrapLayout backgroundColor="#fff">
         <button @tap="showInfo">asdcfvf</button>
@@ -26,7 +32,6 @@
         </AbsoluteLayout>
     </WrapLayout> -->
 </template>
-
 <script>
 export default {
     props: {
@@ -44,32 +49,54 @@ export default {
     },
 };
 </script>
-
 <style scoped>
-.category {
-    width: 100%;
-    text-align: center;
-    margin-top: 17px;
-    font-size: 10px;
-    color: #808080;
-    font-family: 'Montserrat', sans-serif;
-    font-weight: 600;
-}
-
-.titular-box {
-    margin-left: 9%;
-    width: 82%;
-    background-color: #fff;
-}
-
-.titular-ppal {
-    text-align: center;
-    font-size: 19px;
-    color: #000;
-    font-family: 'Bitter', serif;
-}
-
-.img-ppal {
-    width: 100%;
-}
+    .category {
+        width: 100%;
+        text-align: center;
+        margin-top: 17px;
+        font-size: 10px;
+        color: #808080;
+        font-family: 'Montserrat', sans-serif;
+        font-weight: 600;
+    }
+    .box-play {
+        margin-top: 260px;
+        width: 100%;
+        text-align: center;
+    }
+    .titular-videos {
+        font: normal normal normal 19px/25px Bitter;
+        color: #000000;
+        margin: 30px 32px 0;
+    }
+    .img-ppal {
+        width: 100%;
+    }
+    .box-hash {
+        font-size: 11px;
+  		font-family: 'Montserrat', sans-serif;
+  		font-weight: 500;
+		line-height: 14px;
+        color: #808080;
+        border-color: #C2C2C2;
+        border-width: 1 1 1 1;
+        padding: 14px 40px;
+        margin: 40px 32px 0;
+    }
+    .box-category {
+        font-size: 10px;
+  		font-family: 'Montserrat', sans-serif;
+  		font-weight: 600;
+		line-height: 13px;
+        color: #808080;
+        border-color: #C2C2C2;
+        border-width: 0 0 0 1;
+        padding: 15px 26px;
+        margin: 40px 0 0;
+    }
+    .save {
+        position: absolute;
+        right: 0;
+        top: 40px;
+    }
 </style>
